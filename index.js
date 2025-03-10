ac70d4d4114ec428b358b5007f35038bdc2d05ab
Johnson Alala <jonesalala95@gmail.com>
2025-03-11 00:44:37 +0300

    completed the assignment

---
+++ index.js
@@ -0,0 +1,10 @@
+// Creating functin expression for divide
+const divide = function (a, b) {
+  return a / b;
+};
+
+//creating an arrow function for square
+const square = (x) => x * x;
+
+//creating an arrow function for add
+const add = (a, b) => a + b;
